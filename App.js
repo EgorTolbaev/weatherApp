3255a54157a3887a3bd82ddd1837b6f47b26451c
EgorTolbaev <egor05.09.97@gmail.com>
2021-05-10 16:47:16 +0500

    Получение геопозиции и запрос разрешения на доступ к геопозиции

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import * as Location from 'expo-location';
 import Loading from './Loading';
 
-export default function App() {
-  return (
-    <Loading />
-  );
+export default class extends React.Component {
+
+  state = {
+    isLoading: true
+  }
+
+  getLocation = async () => {
+    try{
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.setState({isLoading: false});
+      // TODO: Сделать запрос к API
+    } catch(error){
+      Alert.alert('Не могу определить местоположение', "Очень грустно :(");
+    }
+  }
+  
+  componentDidMount(){
+    this.getLocation();
+  }
+
+  render(){
+    const {isLoading} = this.state;
+    return(
+      isLoading ? <Loading /> : null
+    );
+  }
 }
