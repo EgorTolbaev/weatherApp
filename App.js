63dc6c96709c37b87f5d6351bce5b0cefe3d304f
EgorTolbaev <egor05.09.97@gmail.com>
2023-08-23 15:08:38 +0500

    update

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Loading from './Loading';
 import Weather from './Weather';
 import axios from 'axios';
 
-const API_KEY = '71df472aef68a720f01a54a6ab730d30'
+const API_KEY = ''
 
 export default class extends React.Component {
 
@@ -14,8 +14,8 @@ export default class extends React.Component {
     isLoadingDaily: true
   }
 
-  getWeather = async (latitude, longitude) =>{
-    const {data: {main: {temp, temp_min, temp_max, humidity}, weather, name, wind: { speed }}} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=ru`);
+  getWeather = async (latitude, longitude) => {
+    const { data: { main: { temp, temp_min, temp_max, humidity }, weather, name, wind: { speed } } } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=ru`);
     this.setState({
       isLoading: false,
       temp: temp,
@@ -29,8 +29,8 @@ export default class extends React.Component {
     });
   }
 
-  getWeatherDaily = async (latitude, longitude) =>{
-    const {data: {daily}} = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${API_KEY}&units=metric`);
+  getWeatherDaily = async (latitude, longitude) => {
+    const { data: { daily } } = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,minutely,hourly,alerts&appid=${API_KEY}&units=metric`);
     this.setState({
       isLoadingDaily: false,
       daily: daily,
@@ -39,24 +39,24 @@ export default class extends React.Component {
 
 
   getLocation = async () => {
-    try{
+    try {
       await Location.requestPermissionsAsync(); // requestForegroundPermissionsAsync();
-      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
       this.getWeatherDaily(latitude, longitude);
-    } catch(error){
+    } catch (error) {
       Alert.alert('Не могу определить местоположение', "Очень грустно :(");
     }
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.getLocation();
   }
 
-  render(){
-    const {isLoading, isLoadingDaily, temp, tempMin, humidity, tempMax, location, condition, descriptionWeather, wind, daily} = this.state;
-    return(
-      isLoading || isLoadingDaily ? <Loading /> : <Weather temp={Math.round(temp)} tempMin={Math.round(tempMin)} humidity={humidity} tempMax={Math.round(tempMax)}  location={location} condition={condition} descriptionWeather={descriptionWeather} wind={wind} daily={daily}/>
+  render() {
+    const { isLoading, isLoadingDaily, temp, tempMin, humidity, tempMax, location, condition, descriptionWeather, wind, daily } = this.state;
+    return (
+      isLoading || isLoadingDaily ? <Loading /> : <Weather temp={Math.round(temp)} tempMin={Math.round(tempMin)} humidity={humidity} tempMax={Math.round(tempMax)} location={location} condition={condition} descriptionWeather={descriptionWeather} wind={wind} daily={daily} />
     );
   }
 }
